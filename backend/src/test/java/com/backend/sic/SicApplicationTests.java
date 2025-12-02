b4012d4d4ecfc86a929b251451a44f32eda54060
Khalid kiaa <kiaakhalid4@gmail.com>
2025-12-02 09:02:21 +0100

    ajouter le project backend

---
+++ backend/src/test/java/com/backend/sic/SicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.sic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
