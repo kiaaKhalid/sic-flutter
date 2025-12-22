d931b480fda721168cee840335c21c3b9a6c6de0
Khalid kiaa <kiaakhalid4@gmail.com>
2025-12-22 20:41:39 +0100

    refactor(widgets): enhance AuthTextField component

--- front-end/lib/widgets/auth_text_field.dart
+++ front-end/lib/widgets/auth_text_field.dart
@@ -5,6 +5,8 @@ class AuthTextField extends StatefulWidget {
   final TextEditingController controller;
   final bool obscure;
   final TextInputType? keyboardType;
+  final String? semanticLabel;
+  final String? Function(String?)? validator; // CRUCIAL : Pour recevoir la logique de test
 
   const AuthTextField({
     super.key,
@@ -12,6 +14,8 @@ class AuthTextField extends StatefulWidget {
     required this.controller,
     this.obscure = false,
     this.keyboardType,
+    this.semanticLabel,
+    this.validator,
   });
 
   @override
@@ -29,22 +33,35 @@ class _AuthTextFieldState extends State<AuthTextField> {
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
-      controller: widget.controller,
-      obscureText: _obscure,
-      keyboardType: widget.keyboardType,
-      style: const TextStyle(color: Colors.white),
-      decoration: InputDecoration(
-        labelText: widget.label,
-        suffixIcon: widget.obscure
-            ? IconButton(
-                onPressed: () => setState(() => _obscure = !_obscure),
-                icon: Icon(
-                  _obscure ? Icons.visibility_off : Icons.visibility,
-                  color: const Color(0xFF757575),
-                ),
-              )
-            : null,
+    return Semantics(
+      label: widget.semanticLabel ?? widget.label,
+      textField: true,
+      child: TextFormField( // DOIT ÊTRE TextFormField
+        controller: widget.controller,
+        obscureText: _obscure,
+        keyboardType: widget.keyboardType,
+        validator: widget.validator, // LIAISON : Transmet la validation au Form
+        style: const TextStyle(color: Colors.white),
+        decoration: InputDecoration(
+          labelText: widget.label,
+          labelStyle: const TextStyle(color: Colors.white70),
+          filled: true,
+          fillColor: const Color(0xFF1E1E1E),
+          errorStyle: const TextStyle(color: Colors.redAccent), // Style du message d'erreur
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(12),
+            borderSide: BorderSide.none,
+          ),
+          suffixIcon: widget.obscure
+              ? IconButton(
+                  onPressed: () => setState(() => _obscure = !_obscure),
+                  icon: Icon(
+                    _obscure ? Icons.visibility_off : Icons.visibility,
+                    color: const Color(0xFF757575),
+                  ),
+                )
+              : null,
+        ),
       ),
     );
   }
